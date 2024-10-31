6ee97ed5d0651470bafe959c7077bbcad2cb2d1a
ishansuhail <ishansuhail28@gmail.com>
2024-10-31 14:12:35 -0400

    professional and customer sign up

--- HandyMatch/src/authentication/SignUp.jsx
+++ HandyMatch/src/authentication/SignUp.jsx
@@ -2,22 +2,37 @@ import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from "react-router-dom";
 import { auth } from "./firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUp = ({ onToggle }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [userType, setUserType] = useState("Customer");
   const navigate = useNavigate()
 
-  const handleSubmit = (e) => {
+
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
       alert("Passwords do not match");
       return;
     }
+    const status = await signUp(email, password)
+
+    console.log(status)
+
     alert(`Sign up with Email: ${email}, Password: ${password}`);
+    
+    
     navigate('/home')
   };
+
+  
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
@@ -57,13 +72,25 @@ const SignUp = ({ onToggle }) => {
               required
             />
           </div>
+          <div className="mb-3">
+           
+          <div className = "mb-3 d-flex flex-column align-items-center">
+            <label className="form-label">Are you a...?</label>
+            <select className = "form-select" style={{ backgroundColor: "transparent" }} onChange={(e) => setUserType(e.target.value)}>
+              <option value="Customer">Customer</option>
+              <option value="Professional">Professional</option>
+            </select>
+          </div>
+
+          </div>
           <button type="submit" className="btn btn-primary w-100">
             Sign Up
           </button>
         </form>
         <div className="mt-3 text-center">
-          <a href="#" className="text-secondary" onClick={onToggle}>Already have an account?</a>
+          <a href="/login" className="text-secondary" onClick={onToggle}>Already have an account?</a>
         </div>
+        
       </div>
     </div>
   );
